05b59f8020f93d9d08de35f5708dfd19866d30f8
HusbanShah97 <husbanshah97@gmail.com>
2024-07-20 12:24:44 +0500

    initail commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function off() {
+
+    // let image = scr = "img/bulb off.jpg" 
+    let pic = document.getElementById("images");
+      pic.src = 'img/bulb off.jpg';
+    
+    
+}
+
+function on() {
+    
+    let pic = document.getElementById("images");
+   pic.src = 'img/bulb on.jpg';  
+    
+}
